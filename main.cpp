85898c797e5c1d8599386efe413278f17f894564
tonju <152599126+tonju1124@users.noreply.github.com>
2024-05-20 14:44:33 +0800

    feat:: package

--- main.cpp
+++ main.cpp
@@ -3,27 +3,69 @@
 #include <string>
 
 using namespace std;
-class Person {
-
+class Person
+{
 };
 
-class Manager {
-
+class Manager
+{
 };
 
-class Customer {
-
+class Customer
+{
 };
 
-class Trainer{
-
+class Trainer
+{
 };
 
-class Package{
+class Package
+{
+private:
+    string id;
+    double price;
+    string duration;
 
+public:
+    string getid()
+    {
+        return id;
+    }
+
+    double getPrice()
+    {
+        return price;
+    }
+
+    string getDuration()
+    {
+        return duration;
+    }
+
+    Package(string id, double price, string duration)
+    {
+        this->id = id;
+        this->price = price;
+        this->duration = duration;
+    }
+
+    void package_display()
+    {
+        cout << "========================================" << endl;
+        cout << "             Package Details" << endl;
+        cout << "========================================" << endl;
+        cout << "ID" << ": " << getid() << endl;
+        cout << "Price" << ": " << fixed << setprecision(2) << getPrice() << endl;
+        cout << "Duration" << ": " << getDuration() << endl;
+    }
 };
 
-
-int main(){
-
+int main()
+{
+    Package package[] = {
+        Package("1", 160.00, "Monthly"),
+        Package("2", 300.00, "2Month"),
+        Package("3", 640.00, "6Month"),
+        Package("4", 1600.00, "Annually")};
+    package->package_display();
 }
